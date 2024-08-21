7f9c8fe7641703a3b6dbfeffe86fa46e2bb57443
Onnisut1 <onnisut1@gmail.com>
2024-08-21 14:28:41 +0300

    Added Login.js, updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 // Tuodaan kaikki komponentit sovellukseen
 import Register from './components/Register';
+import Login from './components/Login';
 import './App.css';
 
 // App-komponentti on sovelluksen pääkomponentti
@@ -11,6 +12,8 @@ function App() {
             <h1>Käyttäjien hallintajärjestelmä</h1>
 {/* Rekisteröintikomponentti */}
             <Register />
+{/* Kirjautumiskomponentti*/}
+            <Login />
         </div>
     );
 }
