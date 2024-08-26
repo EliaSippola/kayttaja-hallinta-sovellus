50c7216c1f54dc39c2b8ebd2247aad14b259f0ca
Onnisut1 <onnisut1@gmail.com>
2024-08-26 10:09:36 +0300

    Added Router and routes to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 // src/App.js
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link ,Routes} from 'react-router-dom';
 // Tuodaan kaikki komponentit sovellukseen
 import Register from './components/Register';
 import Login from './components/Login';
@@ -9,15 +10,41 @@ import './App.css';
 // App-komponentti on sovelluksen pääkomponentti
 function App() {
     return (
-        <div className="App">
-            <h1>Käyttäjien hallintajärjestelmä</h1>
-{/* Rekisteröintikomponentti */}
-            <Register />
-{/* Kirjautumiskomponentti */}
-            <Login />
-{/* Käyttäjienhallintakomponentti */}
-            <UserManagement />
-        </div>
+        <Router>
+            <div className="App">
+                <h1>Käyttäjien hallintajärjestelmä</h1>
+                {/* Navigointipalkki */}
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Kotisivu</Link>
+                        </li>
+                        <li>
+                            <Link to="/rekisteröinti">Rekisteröinti</Link>
+                        </li>
+                        <li>
+                            <Link to="/kirjautuminen">Kirjautuminen</Link>
+                        </li>
+                        <li>
+                            <Link to="/käyttäjät">Käyttäjien hallinta</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                {/* Reititykset */}
+                <Routes>
+                    <Route path="/" element={
+                        <div>
+                            <h2>Tervetuloa käyttäjien hallintajärjestelmään</h2>
+                            <p>Ole hyvä ja navigoi käyttämällä yllä olevaa valikkoa.</p>
+                        </div>
+                    } />
+                    <Route path="/rekisteröinti" element={<Register />} />
+                    <Route path="/kirjautuminen" element={<Login />} />
+                    <Route path="/käyttäjät" element={<UserManagement />} />
+                </Routes>
+            </div>
+        </Router>
     );
 }
 export default App;
