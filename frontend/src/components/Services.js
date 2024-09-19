8dd22be5d90fc540534d24411cc3468d6d683bf4
Onnisut1 <onnisut1@gmail.com>
2024-09-19 14:04:57 +0300

    Added cat fact function to services

--- frontend/src/components/Services.js
+++ frontend/src/components/Services.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
 // src/components/Sevices.js
 
 //Services- komponentti mahdollistaa käyttäjän omien tietojen muokkauksen sekä palveluiden käytön.
-function Services() { 
+function Services() {
+    const [catFact, setCatFact] = useState("");
+
+    const handleClick = async () => {
+        await fetch("https://catfact.ninja/fact")
+        .then((res) => res.json())
+        .then((data) => {
+            setCatFact(data.fact);
+        });
+    } 
+    
     return (
         <div>
             <h1>Palvelusivu</h1>
@@ -10,12 +21,13 @@ function Services() {
             Täällä voit tarkastella ja muokata tietojasi.
             <br></br>
             <br></br>
-            <button onclick={()=>{ alert('Profiilinhallinta tulossa!'); }}>Hallitse profiilia</button >
+            <button onClick={()=>{ alert('Profiilinhallinta tulossa!'); }}>Hallitse profiilia</button >
             <h2>Toiminnot</h2>
             Tarjoamme erilaisia toimintoja kirjautuneille käyttäjille.
             <br></br>
             <br></br>
-            <button onclick={()=>{ alert('Profiilinhallinta tulossa!'); }}>Suorita toiminto</button>
+            <button onClick={handleClick}>Suorita toiminto</button>
+            <p>{catFact}</p>
         </div>
     )
 };
